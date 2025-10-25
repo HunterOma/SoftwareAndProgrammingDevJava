691863aad22f7d096ba1c7f6a9a1d8ace4ef4464
HunterOma <142925776+HunterOma@users.noreply.github.com>
2025-10-24 22:28:16 -0600

    Create familymember1.java

---
+++ familymember1.java
@@ -0,0 +1,25 @@
+public class familyMember1 {
+
+
+    public static void main(String[] args) {
+        String familyName1 = "Name1";
+            int familyAge1 = 11;
+        
+        familyMember fm1=new familyMember( familyName1, familyAge1);
+
+        familyName2 = "name2";
+        familyAge2 = 13;
+
+        familyMember fm2= new familyMember( familyName2, familyAge2);
+
+        familyName3 = "name3";
+        familyAge3 = 15;
+
+        familyMember fm3= new familyMember( familyName3, familyAge3);
+
+
+        System.out.println(fm1);
+        System.out.println(fm2);
+        System.out.println(fm3);
+    }
+    }
