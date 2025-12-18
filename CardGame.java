0fce9377916f73fd1b2840f4561e031f1778c021
HunterOma <142925776+HunterOma@users.noreply.github.com>
2025-12-18 13:22:34 -0700

    Add files via upload

--- CardGame.java
+++ CardGame.java
@@ -1,22 +1,23 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class CardGame {
   public static void main(String[] args) {
+        Deck deck= new Deck(); //Creates the deck for this file using the one in Deck.java I believe.
         
-        ArrayList<Integer> deck = new ArrayList<Integer>();
-        
-        deck.add(c1); //Meant to put the specific cards made in CardsList into an array list.
-        deck.add(c2);
-        deck.add(c3);
-        deck.add(c4);
-        deck.add(c5);
-        deck.add(c6);
-        deck.add(c7);
-        deck.add(c8);
-        deck.add(c9);
-        deck.add(c10);
-        Collections.shuffle(deck); //Attempts to shuffle the deck so the number is random
-        
-        System.out.println(Integer.compare(deck.get(0), deck.get(1))); //Meant to get the 1st and 2nd number randomized in the deck, and compare them to print which number is bigger (1 = win, 0 = tie, -1 = loss)
+        Deck.shuffle(); //Attempts to shuffle the deck so the number is random
+        System.out.println("Your card is " + deck.cards(0).value + "of" + deck.cards(0).suite);
+        System.out.println("Opponents card is " + deck.cards(1).value + "of" + deck.cards(1).suite); //Prints the value and suite of both you and your opponents cards
+        int x = deck.cards(0).value; //Sets the values into specific integers for future, easier reference in the actual game.
+        int y = deck.cards(1).value;
+         if (x > y) {
+          System.out.println("You Win"); //Compares the two values to see which number is larger, and compares them to decide who wins using if/else if/else
+        } else if (x < y) {
+          System.out.println("Opponent Wins");
+        }  else {
+          System.out.println("Tie");
+    }
+  }
 }
-}
+        
+      
