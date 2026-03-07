f0ae08aeab7ac1d17d8149c76f27e968e008333c
HunterOma <142925776+HunterOma@users.noreply.github.com>
2026-03-06 21:56:41 -0700

    Add files via upload

--- CardGame.java
+++ CardGame.java
@@ -1,15 +1,37 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Scanner;
 
 public class CardGame {
-  public static void main(String[] args) {
+  static Cards card1;
+  static Cards card2;
+  static Cards card3;
+  static Cards playerCard;
+  static boolean repeatGame= true; //Establishes starting variables
+  public static void main(String[] args) { 
+    int x;
+    int y;
         Deck deck= new Deck(); //Creates the deck for this file using the one in Deck.java I believe.
-        
-        Deck.shuffle(); //Attempts to shuffle the deck so the number is random
-        System.out.println("Your card is " + deck.cards(0).value + "of" + deck.cards(0).suite);
-        System.out.println("Opponents card is " + deck.cards(1).value + "of" + deck.cards(1).suite); //Prints the value and suite of both you and your opponents cards
-        int x = deck.cards(0).value; //Sets the values into specific integers for future, easier reference in the actual game.
-        int y = deck.cards(1).value;
+        while (repeatGame == true) { // Allows for game repeats
+        deck.shuffle(); //Attempts to shuffle the deck so the number is random
+        card1=deck.remove(0);
+        card2=deck.remove(0);
+        card3=deck.remove(0); //allows for retrieval of removed card values and suitesz
+        Scanner cardQuestion = new Scanner(System.in); //creates scanner for input
+        System.out.println("Would you like to choose card 1 or card 2? Please write your answer in numbers.");
+        int cardChoice = cardQuestion.nextInt();
+          if (cardChoice == 1) { 
+            playerCard = card1;
+          }
+          else if (cardChoice == 2) {
+            playerCard = card2;
+          }
+          else {
+            System.out.println("An error has occured, this is likely because a letter was included, or an incompatable number. Defaulting to card 1");
+            playerCard = card1;
+          } //Allows user to pick their card
+        System.out.println("Your card is " + playerCard.getValue() + " of " + card1.getSuite());
+        System.out.println("Opponents card is " + card2.getValue() + " of " + card2.getSuite()); //Prints the value and suite of both you and your opponents cards
+        x=playerCard.getValue(); //playerCard is what allows the user to use their picked card without complicated code.
+        y=card3.getValue(); //sets the removed cards as x and y values for future reference
          if (x > y) {
           System.out.println("You Win"); //Compares the two values to see which number is larger, and compares them to decide who wins using if/else if/else
         } else if (x < y) {
@@ -17,7 +39,19 @@ public class CardGame {
         }  else {
           System.out.println("Tie");
     }
-  }
+        System.out.println ("Would you like to play again? Please write true or false in all lowercase.");
+        try {
+        boolean playAgain = cardQuestion.nextBoolean(); //Uses previously established scanner to ask the user for game repeats.
+          if (playAgain == false) {
+            repeatGame = false;
+          }
+        } catch (Exception e) { //Catches bugs in case the user inputs information unable to be used by the boolean.
+          System.out.println("Something went wrong, repeating game by default.");
+        }
+      cardQuestion.close(); //Closes scanner for game repeats.
+   }
+   
+ }
 }
         
       
